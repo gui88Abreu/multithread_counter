1a8e6f8b5869d43dfa3e0a5b460c8f59dd8eb608
Guilherme Abreu <guiii88@outlook.com>
2018-05-02 19:57:21 -0300

    It's done, just missing comments

--- src/main.c
+++ src/main.c
@@ -6,12 +6,84 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h> /* dinamic memory*/
+#include <pthread.h> /* threads*/
+#include <unistd.h>
+
+#define N_THREADS 4
+#define N_MAX 64
+
+typedef struct {
+    int thread;
+    unsigned long int *numbers;
+    int length_numbers;
+    int *prime_numbers_amount;
+}dt;
+
+void *thread_function(void *args);
+int is_prime(unsigned long int );
 
 int main() {
+  pthread_t thread[N_THREADS];
+  char c;
+  int j;
+  dt data[N_THREADS];
+  unsigned long int numbers_vector[N_MAX];
+  int prime_numbers_amount;
 
-  int x, y;
+  /* Ler no maximo N_MAX numeros inteiros sem sinal seguidos de um \n*/
+  j = 0;
+  do{
+    scanf("%li", &numbers_vector[j]);
+    c = getchar();
+    j += 1;
+  }while (c != '\n' && j < N_MAX);
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
+  /* Contar quantos numeros primos estao armazenados no vetor prime_number de entradas em N_THREADS processos paralelos*/
+  prime_numbers_amount = 0;
+  for (int i = 0; i < N_THREADS; i++) {
+    data[i].thread = i;
+    data[i].numbers = numbers_vector;
+    data[i].prime_numbers_amount = &prime_numbers_amount;
+    data[i].length_numbers = j;
+    pthread_create(&(thread[i]), NULL, thread_function, &(data[i]));
+  }
+
+  //sleep(1);
+  for(int i = 0; i < N_THREADS; i++){
+    pthread_join(thread[i], NULL);
+  }
+
+  printf("%d\n", prime_numbers_amount);
   return 0;
+}
+
+/* Executa os N_THREADS threads*/
+void *thread_function(void *arg){
+  dt *data = (dt *)arg;
+
+  for(int k = 0; data->thread + k < data->length_numbers; k += N_THREADS){
+    //printf("Executando Thread numero %d\n", data->thread);
+    *data->prime_numbers_amount += is_prime(data->numbers[data->thread + k]);
+  }
+  
+  return NULL;
+}
+
+
+/* Determinar se o numero dado eh primo*/
+/* return 1 caso seja e 0 caso contrario*/
+int is_prime(unsigned long int prime_number){
+  int response = 1;
+  int i = 2;
+
+  if (prime_number < 2) response = 0;
+  
+  while(i <= prime_number/2 && response == 1){
+    if (prime_number%i == 0)
+      response = 0;
+    i++;
+  }
+  
+  return response;
 }
